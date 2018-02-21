c094e545b3bd4f6de6e0c711e93fbcbe26a3dfcc
zivbrachia@gmail.com <zivbrachia@gmail.com>
2018-02-21 23:38:42 +0200

    queue

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 'use strict'
 
-let express = require('express');
-let bodyParser = require("body-parser");
-let router = express.Router();
+const Queue2 = require('./queue2.js');
+let queue2 = new Queue2(5).runQueue();
 
-const ActionToQueue = require("action_to_queue.js");
-const Queue = require('queue.js');
-let queue = new Queue(5).runQueue();
+queue2.add(setPromise(5));
+queue2.add(setPromise2(4));
+queue2.add(setPromise(3));
+queue2.add(setPromise2(2));
+queue2.add(setPromise(1));
 
-router.use(bodyParser.json());
+function setPromise(value) {
+    return () => {
+        return new Promise((resolve, reject) => {
+            // some code here...
+            setTimeout(() => {
+                writeSuccess(value);
+                resolve({ "text": value + " " + "setPromise1" });
+            }, value * 1000);
+        });
+    }
+}
 
-router.get('/stop', function (req, res) {
-    queue.stopQueue();
-    res.json({"message" : "Queue stopped"});
-});
+function setPromise2(value) {
+    return () => {
+        return new Promise((resolve, reject) => {
+            // some code here...
+            let err = new Error(value + " stPromise2");
+            writeFailure(value);
+            reject(err);
+        });
+    }
+}
 
-router.get('/stop/:subQueueId', function (req, res) {
-    queue.stopSubQueue(req.params.subQueueId);
-    res.json({"message" : "Sub Queue " +  req.params.subQueueId + " stopped"});
-});
+function writeSuccess(value) {
+    console.log(Date.now() + " success " + value);
+}
 
-router.get('/start', function (req, res) {
-    queue.runQueue();
-    res.json({"message" : "Queue started"});
-});
+function writeFailure(value) {
+    console.log(Date.now() + " failure " + value)
 
-router.get('/:subQueueId', function (req, res) {
-    res.json("TODO");
-});
-
-router.post('/', function (req, res) {
-    let data = req.body;
-    Promise.resolve().then(function () {
-        queue.resetSubQueueCounter(subQueueId);
-        delete ActionToQueue.ActionsResult[subQueueId];
-
-        let asyncAction = new ActionToQueue(subQueueId);
-        // insert to queue the async object, sub queue id and the callback to activate after async object returned
-        queue.add(asyncAction.buildAction(), subQueueId, ActionToQueue.callbackQueueHandler);
-    });
-    res.json("send to queue");
-    }).catch(function (err) {
-        res.json({"error":err);
-    });
-});
-
-module.exports = router;
+}
